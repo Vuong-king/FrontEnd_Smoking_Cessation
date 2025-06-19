0840903deb02c3b1311518d37fc6b9d46c666ca1
nxnting <nxntin2207@gmail.com>
2025-06-19 21:48:53 +0700

    update error

--- src/pages/user/UserBlogPage.jsx
+++ src/pages/user/UserBlogPage.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { usePostData } from "../../hook/usePostData";
-import BlogDetail from "../../components/user/blog/UserBlogDetail";
+import BlogDetail from "../../components/user/blog/BlogDetail";
 import CreateBlog from "../../components/user/blog/CreateBlog";
 import MyPosts from "../../components/user/blog/MyPosts";
 import FilterSidebar from "../../components/user/blog/FilterSidbar";
