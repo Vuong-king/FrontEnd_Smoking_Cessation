35593c0c1ff794caea4c1c962477a86053138495
Vuong-king <vuong9408@gmail.com>
2025-07-11 15:17:23 +0700

    Successfull Payment

--- src/services/packageService.js
+++ src/services/packageService.js
@@ -3,6 +3,10 @@ import api from '../api';
 const PackageService = {
   getAllPackages: async () => {
     const response = await api.get('/packages');
+    return response.data.packages;
+  },
+    getPackageById: async (id) => {
+    const response = await api.get(`/packages/${id}`);
     return response.data;
   },
 };
